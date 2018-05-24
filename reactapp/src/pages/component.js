8c6133c868997956443a402dd1f73f503499f09b
Jeremy Thiesen <jeremy.thiesen1@gmail.com>
2018-05-23 21:09:45 -0600

    Adding in the form submission for ManageComponent

--- reactapp/src/pages/component.js
+++ reactapp/src/pages/component.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 
 import ajaxWrapper from "../base/ajax.js";
 import Wrapper from '../base/wrapper.js';
+import $ from 'jquery';
 
 class ManageComponent extends Component {
     constructor(props) {
@@ -12,14 +13,33 @@ class ManageComponent extends Component {
         };
 
         this.ajaxCallback = this.ajaxCallback.bind(this);
+        this.formSubmit = this.formSubmit.bind(this);
     }
 
     componentDidMount() {
         ajaxWrapper("GET", "/api/component/" + this.props.id + "/", {}, this.ajaxCallback);
     }
 
-    ajaxCallback(value){
+    handleChange = (e) => {
+       var name = e.target.getAttribute("name");
+       var newState = {data:this.state.data}
+       newState.data[name] = e.target.value;
+       console.log(newState);
+       this.setState(newState);
+    }
+
+    formSubmit() {
+        console.log("Data", this.state.data.name);
+        var data = {name: this.state.data.name, description: this.state.data.description, html: this.state.data.html}
+        console.log(data);
+        ajaxWrapper("POST","/models/modelInstance/home/component/" + this.props.id + "/", data, this.formSubmitCallback)
+    }
+
+    formSubmitCallback (value) {
         console.log(value);
+    }
+
+    ajaxCallback(value){
 
         this.setState({
             data: value.component,
@@ -39,22 +59,22 @@ class ManageComponent extends Component {
                 <a href="/components/" >back to list</a>
                 <br/><br/>
 
-                <form action="POST" >
+
                     <label>Name</label>
-                    <input className="form-control" name="name" value={data.name} />
+                    <input className="form-control" name="name" value={data.name} onChange={this.handleChange} />
                     <br/>
 
                     <label>Description</label>
-                    <textarea className="form-control" name="description">{data.description}</textarea>
+                    <textarea className="form-control" name="description" value={data.description} onChange={this.handleChange}></textarea>
                     <br/>
 
                     <label>Source</label>
-                    <textarea className="form-control" name="html">{data.html}</textarea>
+                    <textarea className="form-control" name="html" value={data.html} onChange={this.handleChange}></textarea>
                     <br/>
 
-                    <input className="btn btn-success" name="save" value="Save" />
+                    <input type="submit" className="btn btn-success" name="save" value="Save" onClick={this.formSubmit} />
                     <br/><br/>
-                </form>
+
             </div>;
         }
 
