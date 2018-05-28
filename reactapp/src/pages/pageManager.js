a95152155654375310759b116c592f0644587aee
GoblinGobbler <dmiller89@gmail.com>
2018-05-28 16:06:06 -0600

    PageComponent modal for page

--- reactapp/src/pages/pageManager.js
+++ reactapp/src/pages/pageManager.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 import ajaxWrapper from "../base/ajax.js";
 import Wrapper from '../base/wrapper.js';
 import Card from '../components/card.js';
+import Modal from '../components/modal.js';
 
 import Playground from "component-playground";
 import ReactDOM from "react-dom";
@@ -14,7 +15,10 @@ class PageManager extends Component {
             loaded: false,
             data: {},
             components: [],
-            pageComponents: []
+            pageComponents: [],
+            activePageComponent: null,
+            activeComponent: null,
+            showComponent: false
         };
 
         this.ajaxCallback = this.ajaxCallback.bind(this);
@@ -75,20 +79,46 @@ class PageManager extends Component {
         });
     }
 
+    addComponent(data){
+        console.log(data);
+        this.setState({
+            activePageComponent: {id: -1, 'component':data.id, 'page': this.props.id},
+            activeComponent: data,
+            showComponent: true});
+    }
+
+    editComponent(data){
+        console.log(data);
+        this.setState({
+            activePageComponent: data,
+            activeComponent: data['component'],
+            showComponent: true});
+    }
+
+    hideComponent(){
+        this.setState({showComponent: false});
+    }
+
     render() {
         var components = [];
         for (var i = 0; i < this.state.components.length; i++){
             var data = this.state.components[i]['component'];
-            var componentSmall = <Card name={data.name} description={data.description} button='Add' button_type="primary" />;
+            var componentSmall = <Card name={data.name} description={data.description} onClick={this.addComponent.bind(this, data)} button='Add' button_type="primary" />;
 
             components.push(componentSmall);
         }
         var pageComponents = [];
         for (var i = 0; i < this.state.pageComponents.length; i++){
-            var data = this.state.pageComponents[i];
-            var componentSmall = <Card name={data.name} description={data.description} button='Edit' button_type="primary" />;
+            var data = this.state.pageComponents[i]['pagecomponent'];
+            var componentSmall = <Card name={data.name} description={data.description} onClick={this.editComponent.bind(this, data)} button='Edit' button_type="primary" />;
 
             pageComponents.push(componentSmall);
+        }
+
+        var modal = null;
+        if (this.state.activeComponent){
+            modal = <PageComponentModal key={this.state.activePageComponent.id} component={this.state.activeComponent} data={this.state.activePageComponent}
+                show={this.state.showComponent} handleClose={this.hideComponent.bind(this)} />;
         }
 
         var content = null;
@@ -124,6 +154,8 @@ class PageManager extends Component {
             <input type="submit" className="btn btn-success" name="save" value="Save" onClick={this.formSubmit} />
             <br/><br/>
 
+            {modal}
+
         </div>;
 
         return (
@@ -131,5 +163,82 @@ class PageManager extends Component {
         );
     }
 }
+
+
+
+class PageComponentModal extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            data: {}
+        };
+
+        this.formSubmit = this.formSubmit.bind(this);
+    }
+
+    componentDidMount() {
+        var data = this.props.data;
+        data.data = JSON.stringify(data.data);
+        this.setState({data: this.props.data});
+    }
+
+    handleChange = (e) => {
+       var name = e.target.getAttribute("name");
+       var newState = {data:this.state.data}
+       newState.data[name] = e.target.value;
+       console.log(newState);
+       this.setState(newState);
+    }
+
+    formSubmit() {
+        var data = this.state.data;
+        console.log(data);
+        if (this.props.data.id > -1){
+            ajaxWrapper("POST","/models/getModelInstanceJson/home/pagecomponent/" + this.props.data.id + "/", data, this.formSubmitCallback.bind(this));
+        } else {
+            ajaxWrapper("POST","/models/getModelInstanceJson/home/pagecomponent/", data, this.formSubmitCallback.bind(this));
+        }
+    }
+
+    formSubmitCallback (value) {
+        console.log(value);
+
+    }
+
+    render() {
+        var content = null;
+        var data = this.state.data;
+
+        var title = "Modal heading";
+        var content =
+        <div className="col-sm-12">
+            <h2>Page Component</h2>
+            <br/><br/>
+
+            <label>Order</label>
+            <input className="form-control" name="order" value={data.order} onChange={this.handleChange} />
+            <br/>
+
+            <label>Data URL</label>
+            <input className="form-control" name="data_url" value={data.data_url} onChange={this.handleChange} />
+            <br/>
+
+            <label>Data</label>
+            <textarea className="form-control" name="data" value={data.data} onChange={this.handleChange} ></textarea>
+            <br/>
+
+            <input type="submit" className="btn btn-success" name="save" value="Save" onClick={this.formSubmit.bind(this)} />
+            <br/><br/>
+
+        </div>;
+
+
+        return (
+            <Modal show={this.props.show} title={title} content={content} onHide={this.props.handleClose}
+                handleChange={this.handleChange.bind(this)} formSubmit={this.formSubmit}/>
+        );
+    }
+}
+
 
 export default PageManager;
