c45705e38d5627f0c7aa5b9861080e9e92ff9aef
Jeremy Thiesen <jeremy.thiesen1@gmail.com>
2018-05-31 16:03:03 -0600

    Rewrote ComponentList to use the List Component as part of the transition to the site running off of Component Madness.

--- reactapp/src/pages/componentList.js
+++ reactapp/src/pages/componentList.js
@@ -3,46 +3,18 @@ import React, { Component } from 'react';
 import ajaxWrapper from "../base/ajax.js";
 import Wrapper from '../base/wrapper.js';
 import Card from '../library/card.js';
+import List from '../library/list.js';
+
+
 
 class ComponentList extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            loaded: false,
-            components: []
-        };
-
-        this.ajaxCallback = this.ajaxCallback.bind(this);
-    }
-
-    componentDidMount() {
-        ajaxWrapper("GET", "/api/components/", {}, this.ajaxCallback);
-    }
-
-    ajaxCallback(value){
-        console.log(value);
-
-        this.setState({
-            components: value.components,
-            loaded: true
-        });
-    }
 
     render() {
-        var content = [];
-        for (var i = 0; i < this.state.components.length; i++){
-            var data = this.state.components[i];
-            var componentSmall = <Card name={data.name} description={data.description} link={"/component/" + data.id} button='Edit' button_type="primary" />;
-
-            content.push(componentSmall);
-        }
-        var componentSmall = <Card name="Something New?" description="Add A New Component" link="/component/-1/" button="Create New" button_type="success" />;
-
-        content.push(componentSmall);
-
-
+        var lastInstanceData = {'name':"Something New?", 'description':"Add A New Component", 'link':"/component/", 'button':"Create New", 'button_type':"success"};
+        var extraInfo = {'button_type':'primary', 'button':'Edit', 'link':'/component/{id}/'};
         return (
-            <Wrapper loaded={this.state.loaded} content={content} />
+
+            <List dataUrl={"/models/getModelInstanceJson/home/component/"} component={Card} objectName={'component'} extraInfo={extraInfo} lastInstanceData={lastInstanceData} />
         );
     }
 }
