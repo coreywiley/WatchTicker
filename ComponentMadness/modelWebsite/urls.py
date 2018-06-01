bb17fee8de8dcc13a146d9252315bf9521c225a1
Jeremy Thiesen <jeremy.thiesen1@gmail.com>
2018-06-01 14:33:44 -0600

    Converting ModelWebsite to all json responses.

--- ComponentMadness/modelWebsite/urls.py
+++ ComponentMadness/modelWebsite/urls.py
@@ -1,21 +1,14 @@
 from django.conf.urls import url
 
-from modelWebsite.views import getModels, getModelValues, getModelInstance, \
-    getModelInstanceJson, deleteModelInstance, writeComponents
+from modelWebsite.views import getModels, getModelInstanceJson, deleteModelInstance, writeComponents
 
 urlpatterns = [
     url(r'^getModelInstanceJson/(?P<appLabel>[a-zA-Z]+)/(?P<modelName>[a-zA-Z_]+)/(?P<id>[0-9]+)/delete/', deleteModelInstance, name='deleteModelInstance'),
     url(r'^getModels/', getModels, name='getModels'),
-    url(r'^getModelValues/(?P<appLabel>[a-zA-Z]+)/(?P<modelName>[a-zA-Z_]+)/(?P<offset>[0-9]+)/', getModelValues, name='getModelValues'),
-
-    url(r'^modelInstance/(?P<appLabel>[a-zA-Z]+)/(?P<modelName>[a-zA-Z_]+)/(?P<id>[0-9]+)/', getModelInstance, name='getModelInstance'),
-    url(r'^modelInstance/(?P<appLabel>[a-zA-Z]+)/(?P<modelName>[a-zA-Z_]+)/', getModelInstance, name='getModelInstance'),
 
     url(r'^getModelInstanceJson/(?P<appLabel>[a-zA-Z]+)/(?P<modelName>[a-zA-Z_]+)/(?P<id>[{}a-zA-Z._]+)/', getModelInstanceJson, name='getModelInstance'),
     url(r'^getModelInstanceJson/(?P<appLabel>[a-zA-Z]+)/(?P<modelName>[a-zA-Z_]+)/(?P<id>[0-9]+)/', getModelInstanceJson, name='getModelInstance'),
     url(r'^getModelInstanceJson/(?P<appLabel>[a-zA-Z]+)/(?P<modelName>[a-zA-Z_]+)/', getModelInstanceJson, name='getModelInstance'),
-
-
 
     url(r'^writeComponents/$', writeComponents, name = "writeComponents")
 
