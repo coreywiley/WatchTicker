1d0f6e741e88c91378a16920db0fdfdf0d810418
GoblinGobbler <dmiller89@gmail.com>
2018-06-08 17:04:04 -0600

    Global state handling in Compiler.  Gets URL context from ClientApp

--- reactapp/src/compilerTemplate.js
+++ reactapp/src/compilerTemplate.js
@@ -3,7 +3,13 @@ import {{IMPORTS}} from './library';
 
 
 class Compiler extends Component {
-    resolveComponentsInProps(pageComponent){
+    componentDidMount() {
+        this.setState({...this.props.globalState});
+    }
+
+    resolveComponents(pageComponent){
+        var pageComponent = JSON.parse(pageComponent);
+
         var props = pageComponent.component.componentProps;
         var data = pageComponent.data;
 
@@ -12,12 +18,43 @@ class Compiler extends Component {
 
             if (prop.type == "Component"){
                 data[prop.name] = this.getComponentByName(data[prop.name], {});
+            } else if (prop.type == "Component List"){
+                for (var i=0; i<data[prop.name].length; i++){
+                    data[prop.name][i] = this.getComponentByName(data[prop.name][i], {});
+                }
+            } else if (prop.type == "String") {
+                data[prop.name] == this.parseGlobalData(prop.type, data[prop.name]);
             }
         }
         return data;
     }
 
-    setGlobalState(componentName,value) {
+    parseGlobalData(type, data) {
+        console.log("Parsing Gobal Var", data, type);
+        //Split string into pieces using the variable starting char
+        var dataSplit = data.split('{');
+        //Add initial text to output
+        var cleaned = dataSplit[0];
+        //Search through string pieces to find closing tag
+        for (var i=1; i<dataSplit.length; i++){
+            var innerSplit = dataSplit.split('}');
+            if (innerSplit.length > 1){
+                var variable = innerSplit[0];
+                if (variable.startsWith("GLOBAL")){
+                    var value = this.state[variable];
+                    cleaned += value + innerSplit[1];
+                } else {
+                    cleaned += "{"+variable+"}" + innerSplit[1];
+                }
+            } else {
+                cleaned += innerSplit[0];
+            }
+        }
+
+        return cleaned;
+    }
+
+    setGlobalState(componentName, value) {
         console.log("Global State Change",componentName,value);
         var newState = {};
         newState[componentName] = value;
@@ -34,7 +71,7 @@ class Compiler extends Component {
         for (var i=0; i<page.pageComponents.length; i++){
             var pageComponent = page.pageComponents[i].pagecomponent;
 
-            var data = this.resolveComponentsInProps(pageComponent);
+            var data = this.resolveComponents(JSON.stringify(pageComponent));
             let ComponentPointer = this.getComponentByName(pageComponent.component.name, pageComponent.data);
 
             content.push(
