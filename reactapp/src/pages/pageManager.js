dbd76dcf02a3b5cab099227a56fe178fbec9c79e
GoblinGobbler <dmiller89@gmail.com>
2018-06-08 17:04:36 -0600

    Modifying pageManager wrapping for readability

--- reactapp/src/pages/pageManager.js
+++ reactapp/src/pages/pageManager.js
@@ -36,15 +36,31 @@ class PageManager extends Component {
         var title = "Modal heading";
 
         var pageComponentComponents = [TextInput, TextInput, TextArea]
-        var pageComponentComponentProps = [{'label':'Order','name':'order'}, {'label':'Data Url','name':'data_url'}, {'label':'Data','name':'data'}];
+        var pageComponentComponentProps = [
+            {'label':'Order','name':'order'},
+            {'label':'Data Url','name':'data_url'},
+            {'label':'Data','name':'data'}
+        ];
         var pageComponentSubmitUrl = "/api/home/pagecomponent/" + this.state.activePageComponent.id + "/?related=component";
         var pageComponentDeleteUrl = "/api/home/pagecomponent/" + this.state.activePageComponent.id + "/delete/";
-        var pageComponentData = JSON.stringify(this.state.activePageComponent.data);
-        var pageComponentDefaults = {'modal':false, 'order':this.state.activePageComponent.order, 'data_url': this.state.activePageComponent.data_url, 'data':pageComponentData, 'pagecomponent_id': this.state.activePageComponent.id}
+        var pageComponentData = this.state.activePageComponent.data;
+        var pageComponentDefaults = {
+            'modal':false,
+            'order':this.state.activePageComponent.order,
+            'data_url': this.state.activePageComponent.data_url,
+            'data':pageComponentData,
+            'pagecomponent_id': this.state.activePageComponent.id
+        };
 
-        var content = <Form setGlobalState={this.setGlobalState} globalStateName={'activePageComponent'} title={"Page Component"} components={pageComponentComponents} componentProps={pageComponentComponentProps} submitUrl={pageComponentSubmitUrl} deleteUrl={pageComponentDeleteUrl} defaults={pageComponentDefaults}/>;
+        console.log(this.state.activePageComponent.data);
+        console.log(pageComponentData);
 
-         var modal = <Modal show={this.state.activePageComponent.modal} title={title} content={content} setGlobalState={this.setGlobalState} globalStateName={'activePageComponent'} />
+        var content = <Form setGlobalState={this.setGlobalState} globalStateName={'activePageComponent'}
+            title={"Page Component"} components={pageComponentComponents} componentProps={pageComponentComponentProps}
+            submitUrl={pageComponentSubmitUrl} deleteUrl={pageComponentDeleteUrl} defaults={pageComponentDefaults}/>;
+
+         var modal = <Modal show={this.state.activePageComponent.modal} title={title} content={content}
+             setGlobalState={this.setGlobalState} globalStateName={'activePageComponent'} />
 
 
         var content = null;
@@ -60,14 +76,33 @@ class PageManager extends Component {
             deleteUrl = "/api/home/page/" + this.props.id + "/delete/";
         }
 
-        var pageForm = <Form components={FormComponents} objectName={'page'} setGlobalState={this.setGlobalState} dataUrl={submitUrl} componentProps={FormComponentProps} submitUrl={submitUrl} deleteUrl={deleteUrl} defaults={{'name':'','url':'','component':this.props.id}}/>;
+        var pageForm = <Form components={FormComponents} objectName={'page'} setGlobalState={this.setGlobalState}
+            dataUrl={submitUrl} componentProps={FormComponentProps} submitUrl={submitUrl} deleteUrl={deleteUrl}
+            defaults={{'name':'','url':'','component':this.props.id}}/>;
 
-        var buttonComponentProps = {'components':[], 'componentProps':[], 'submitUrl':'/api/home/pagecomponent/', 'defaults':{'modal':true,'page':this.props.id,'component':'{id}', 'order':0, 'data_url':'', 'data':''}, 'setGlobalState':this.setGlobalState, 'globalStateName':'activePageComponent', 'objectName':'pagecomponent'}
-        var componentListDataMapping = {'name':'{name}', 'description':'{description}', 'buttonComponent': Form, 'buttonComponentProps':buttonComponentProps}
-        var componentList = <List title={<label>Components</label>} dataUrl={'/api/home/component/'} component={Card} objectName={'component'} dataMapping={componentListDataMapping} />
+        var buttonComponentProps = {
+            'components':[], 'componentProps':[], 'submitUrl':'/api/home/pagecomponent/',
+            'defaults':{'modal':true,'page':this.props.id,'component':'{id}', 'order':0,
+            'data_url':'', 'data':''}, 'setGlobalState':this.setGlobalState, 'globalStateName':'activePageComponent',
+            'objectName':'pagecomponent'
+        }
+        var componentListDataMapping = {
+            'name':'{name}', 'description':'{description}', 'buttonComponent': Form,
+            'buttonComponentProps':buttonComponentProps
+        }
+        var componentList = <List title={<label>Components</label>} dataUrl={'/api/home/component/'}
+            component={Card} objectName={'component'} dataMapping={componentListDataMapping} />
 
-        var pageComponentListDataMapping = {'name':['component','name'], 'description':'{data}', 'button':'Edit', 'button_type':'primary', 'onClick':this.setGlobalState, 'globalStateName':'activePageComponent', 'id':'{id}', 'data':'{data}','order':'{order}','data_url':'{data_url}', 'modal':true}
-        var pageComponentList = <List title={<label>Existing Components</label>} dataUrl={'/api/home/pagecomponent/?page_id=' + this.props.id + '&related=component'} newComponent={this.state.newPageComponent} component={Card} objectName={'pagecomponent'} dataMapping={pageComponentListDataMapping} />
+        var pageComponentListDataMapping = {
+            'name':['component','name'], 'description':'{data}',
+            'button':'Edit', 'button_type':'primary', 'onClick':this.setGlobalState,
+            'globalStateName':'activePageComponent', 'id':'{id}', 'data':'{data}','order':'{order}',
+            'data_url':'{data_url}', 'modal':true
+        }
+        var pageComponentList = <List title={<label>Existing Components</label>}
+            dataUrl={'/api/home/pagecomponent/?page_id=' + this.props.id + '&related=component'}
+            newComponent={this.state.newPageComponent} component={Card} objectName={'pagecomponent'}
+            dataMapping={pageComponentListDataMapping} />;
 
 
         content =
