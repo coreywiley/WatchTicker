1cbe9772a5645cb8d2dd6337f9702bb1ecb2d873
David Miller <dmiller89@gmail.com>
2018-08-02 18:18:49 -0600

    Initial model creation

--- ComponentMadness/home/models.py
+++ ComponentMadness/home/models.py
@@ -7,3 +7,35 @@ from django_extensions.db.fields import CreationDateTimeField
 from user.models import User
 # Create your models here.
 
+
+class Test(models.Model):
+    name = models.CharField(default = "", null = False)
+
+
+class Question(models.Model):
+    test = models.ForeignKey(Test)
+    text = models.TextField(default = "", null = False)
+    options = models.CharField(default = "", null = False)
+
+
+class Answer(models.Model):
+    question = models.ForeignKey(Question, null = False)
+    sid = models.IntegerField(default = 0, null = False)
+
+    text = models.TextField(default = "", null = False)
+
+
+class Analysis(models.Model):
+    answer = models.ForeignKey(Answer, null = False)
+    user = models.ForeignKey(User, null = False)
+
+    score = models.CharField(default = "", null = False)
+
+
+
+class Comment(models.Model):
+    answer = models.ForeignKey(Answer, null = False)
+    user = models.ForeignKey(User, null = False)
+
+    text = models.TextField(default = "", null = False)
+
