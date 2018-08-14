062647452c9154e401375cec3a5c7b6f5af09fa3
Jeremy Thiesen <jeremy.thiesen1@gmail.com>
2018-08-14 14:29:19 -0600

    Admin Page Updates

--- reactapp/src/pages/modelInstances.js
+++ reactapp/src/pages/modelInstances.js
@@ -1,14 +1,17 @@
 import React, { Component } from 'react';
-import {List, Link} from 'library';
-
+import {List, Link, Button} from 'library';
+import Nav from '../projectLibrary/nav.js';
 
 class InstanceList extends Component {
 
     render() {
         var dataMapping = {'link':'/instance/' + this.props.app + '/' + this.props.model + '/{id}/', 'text':'{unicode}', 'cssClass':"list-group-item"};
+
         return (
             <div className="container list-group">
-                <List dataUrl={"/api/" + this.props.app + "/" + this.props.model + "/"} title={'Instances: ' + this.props.app + '/' + this.props.model} component={Link} objectName={this.props.model} dataMapping={dataMapping} />
+              <Nav token={this.props.user_id} logOut={this.props.logOut} />
+              <List dataUrl={"/api/" + this.props.app + "/" + this.props.model + "/"} title={'Instances: ' + this.props.app + '/' + this.props.model} component={Link} objectName={this.props.model} dataMapping={dataMapping} />
+              <Button type={'success'} text={'Add New'} href={"/instance/" + this.props.app + "/" + this.props.model + "/"} />
             </div>
         );
     }
