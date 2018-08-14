a52912e2e8eaf040e0edf433ca02a55df10e2c98
Jeremy Thiesen <jeremy.thiesen1@gmail.com>
2018-08-14 14:25:36 -0600

    Seperated error handler

--- reactapp/src/base/ajax.js
+++ reactapp/src/base/ajax.js
@@ -1,32 +1,32 @@
 import $ from 'jquery';
 
+function error(xhr, status, erro) {
+  console.log("Ajax Failure")
+  console.log(xhr.responseText);
+  console.log(status)
+  console.log(error)
+}
+
 function ajaxWrapper(type, url, data, returnFunc){
     if (type === "POST"){
         data["csrfmiddlewaretoken"] = window.secretReactVars["csrfmiddlewaretoken"];
     }
+    console.log("Url",url);
+      $.ajax({
+          type: type,
+          url: url,
+          data: data,
+          success: function (value) {
+              if (typeof(value) === "object" && "redirect" in value) {
+                  window.location = value['redirect'] + "?redirect=" + window.secretReactVars["BASE_URL"];
+              }
+              returnFunc(value);
+          },
+          error: function(xhr, status, error) {
+              error(xhr,status,error)
+          }
+      });
 
-
-    $.ajax({
-        type: type,
-        url: url,
-        data: data,
-        success: function (value) {
-            console.log(url);
-            console.log(value);
-
-            if (typeof(value) === "object" && "redirect" in value) {
-                window.location = value['redirect'] + "?redirect=" + window.secretReactVars["BASE_URL"];
-            }
-
-            returnFunc(value);
-        },
-        error: function(xhr, status, error) {
-            console.log("Ajax Failure")
-            console.log(xhr.responseText);
-            console.log(status)
-            console.log(error)
-        }
-    });
 }
 
 
