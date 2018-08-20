91f866b793410c45b56b3fa7d0cc08ba11d523b8
Jeremy Thiesen <jeremy.thiesen1@gmail.com>
2018-08-20 12:02:50 -0600

    CM Library Updates

---
+++ reactapp/src/library/displayComponents/emojiSlider.js
@@ -0,0 +1,83 @@
+import React, { Component } from 'react';
+import Wrapper from 'base/wrapper.js';
+
+import {Container, Button, Image, Form, TextInput, Navbar, List, Link, Accordion, Paragraph, RadioButton, TextArea, Header, Select, ColorPicker, EmojiList} from 'library';
+
+import Draggable, {DraggableCore} from 'react-draggable'; // Both at the same time
+
+class EmojiSlider extends Component {
+    constructor(props) {
+      super(props);
+      this.state = {'total_width':0, width:0}
+
+
+
+      this.handleStart = this.handleStart.bind(this);
+      this.handleDrag = this.handleDrag.bind(this);
+      this.handleStop = this.handleStop.bind(this);
+    }
+
+    componentDidMount() {
+      var width = document.getElementById('progress').getBoundingClientRect().width;
+      console.log("Total Width",width)
+      this.setState({'total_width':width})
+    }
+
+    handleStart() {
+      if (this.props.handleStart) {
+        this.props.handleStart();
+      }
+    }
+
+    handleDrag(e, data) {
+      console.log("Dragging!!", data);
+      var width = Math.floor((data.x+50)/this.state.total_width*100);
+      this.setState({'width':width})
+    }
+
+    handleStop() {
+      if (this.props.handleStop) {
+        this.props.handleStop(this.state.width);
+      }
+    }
+
+    render() {
+
+
+      var progressBarStyle = {"width": this.state.width + "%", 'backgroundColor': this.props.progress_bar_color}
+      var draggableStyle = {
+        width: "0px",
+        height: "0px",
+        cursor: "move",
+        fontSize:"40px",
+      }
+
+        var emojiIcon = <Draggable axis="x" onStart={this.handleStart} onDrag={this.handleDrag} onStop={this.handleStop} bounds={{left:-50, right:this.state.total_width - 50}} defaultPosition={{x:-50,y:0}} >
+          <div className={this.props.emoji} style={draggableStyle}></div>
+          </Draggable>;
+        if (this.props.live == false) {
+          var emojiIcon = <Draggable axis="none" bounds={{left:-50, right:this.state.total_width - 50}} defaultPosition={{x:-50 + this.state.total_width*this.props.width/100,y:0}} >
+            <div className={this.props.emoji} style={draggableStyle}></div>
+            </Draggable>;
+          progressBarStyle = {"width": this.props.width + "%", 'backgroundColor': this.props.progress_bar_color}
+        }
+
+        return (
+          <div style={{"padding":"50px", "backgroundColor": this.props.background_color}}>
+            <h2 style={{"textAlign":'center', 'color':this.props.text_color}}>{this.props.prompt}</h2>
+            <div id="container" style={{"paddingRight": "25px", "paddingLeft":"25px", "zIndex":"99999"}}>
+            {emojiIcon}
+            </div>
+            <div className="labels">
+              <span style={{"height":"25px"}}></span>
+            </div>
+            <div id='progress' className="progress">
+              <div className="progress-bar" role="progressbar" style={progressBarStyle}>
+              </div>
+            </div>
+          </div>
+        );
+    }
+}
+
+export default EmojiSlider;
