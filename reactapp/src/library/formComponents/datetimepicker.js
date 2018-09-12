c92526b62d43133143510025e7202301b92ada8b
Jeremy Thiesen <jeremy.thiesen1@gmail.com>
2018-09-12 14:20:23 -0600

    CM: Created a datetime picker for the cm library

--- reactapp/src/library/formComponents/datetimepicker.js
+++ reactapp/src/library/formComponents/datetimepicker.js
@@ -1,13 +1,32 @@
 import React, { Component } from 'react';
 import resolveVariables from 'base/resolver.js';
-import { DateTimePicker } from 'react-widgets';
-import Moment from 'moment';
-import momentLocalizer from 'react-widgets-moment';
+import Datetime from 'react-datetime';
 
-Moment.locale('en')
-momentLocalizer()
+class DateTimePicker extends Component {
+  constructor(props) {
+    super(props);
 
-class DateTime extends Component {
+    this.handlechange = this.handlechange.bind(this);
+  }
+
+    handlechange(e) {
+      var value = '';
+
+      if (this.props.display_time == false) {
+        value = e.format("M/DD/YYYY");
+      }
+      else if (this.props.display_date == false) {
+        value = e.format("hh:mm A");
+      }
+      else {
+        value = e.format("M/DD/YYYY hh:mm A");
+      }
+
+      var newState = {}
+      newState[this.props.name] = value;
+      this.props.setFormState(newState);
+    }
+
     render() {
 
         var layout = '';
@@ -16,10 +35,22 @@ class DateTime extends Component {
         }
         //<input type="text" className="form-control" name={this.props.name} onChange={this.props.handlechange} value={this.props.value} placeholder={this.props.placeholder} />
 
+        var display_time = true;
+        console.log("Display Time", this.props.display_time)
+        if (this.props.display_time == false) {
+          console.log("Display time false!")
+          display_time = false;
+        }
+
+        var display_date = true;
+        if (this.props.display_date == false) {
+          display_date = false;
+        }
+
         return (
               <div className={"form-group " + this.props.layout}>
                 <label>{this.props.label}</label>
-                <DateTimePicker />
+                <Datetime timeFormat={display_time} dateFormat={display_date}  onChange={this.handlechange} defaultValue={this.props.value}/>
 
               </div>
         )
@@ -28,4 +59,4 @@ class DateTime extends Component {
     }
 }
 
-export default DateTime;
+export default DateTimePicker;
