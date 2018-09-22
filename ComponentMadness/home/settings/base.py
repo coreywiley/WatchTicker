aea24840d113ead8e20f4a39d2049ec4bee6465e
GoblinGobbler <dmiller89@gmail.com>
2018-09-22 10:49:24 -0600

    Config

--- ComponentMadness/home/settings/base.py
+++ ComponentMadness/home/settings/base.py
@@ -110,9 +110,9 @@ DATABASES = {
     'default': {
         'CONN_MAX_AGE': 0,
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'componentmadness',
-        'USER': 'madness',
-        'PASSWORD': 'thisissparta',
+        'NAME': 'collab_hackathon',
+        'USER': 'collab',
+        'PASSWORD': 'admin',
         'HOST': '127.0.0.1',
         'PORT': '5432'
     }
