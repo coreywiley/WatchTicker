3d2aa031e1c10a807186dd7892373816091b6400
GoblinGobbler <dmiller89@gmail.com>
2018-09-29 17:19:24 -0600

    Results page

--- reactapp/src/pages/results.js
+++ reactapp/src/pages/results.js
@@ -39,7 +39,7 @@ class ResultPage extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            form: {},
+            form: null,
             elements: {},
             loaded: false,
         };
@@ -77,133 +77,216 @@ class ResultPage extends Component {
     }
 
     render() {
+        var content = null;
         var submissions = [];
         var places = [];
-        var submissionCards = [];
-        var elements = this.state.form['elements'];
-
-        for (var i in this.state.form['submissions']){
-            var submission = this.state.form['submissions'][i]['formsubmission'];
-            var title = submission['searchTerm'];
-            if ('name' in submission['market']){
-                title += " : " + submission['market']['name'];
-            }
-
-            submissions.push([
-                i,
-                submission['market']['name'],
-                submission['data']['address'],
-                submission['data'][elements[11]['formelement']['id']],
-                submission['data'][elements[7]['formelement']['id']],
-                '$' + submission['data'][elements[18]['formelement']['id']] + ' - $'  + submission['data'][elements[19]['formelement']['id']],
-                submission['data'][elements[17]['formelement']['id']],
-                '$' + submission['data'][elements[20]['formelement']['id']] + ' - $'  + submission['data'][elements[21]['formelement']['id']],
-            ]);
-
-            places.push(submission['data']['placeId']);
-
-            submissionCards.push(
-                <div className='card' style={{textAlign:'left', padding:'10px'}}>
-                    <h5>{title}</h5>
-                    <Button css={{marginLeft: "10px"}} inline={true} type={'info'}
-                        text={'View Result'} href={'/project/' + this.props.project + '/results/' + this.props.id + '/submission/' + submission.id + '/'} />
-                </div>
-            );
-        }
-        var headers = [
-            '#',
-            'Buisiness Unit',
-            'Property Address',
-            'Size',
-            'Owned vs. Leased',
-            'NNN Market Rent/SF/YR',
-            'Sale Value - Vacant',
-            'Sale Value - Leaseback'
-        ];
-
+        var title = null;
+        var charts = [];
+        var data = [0,0, 0,0,0, 0,0,0];
         var submissionTable = null;
         var map = null;
-        if (this.state.loaded){
-            map = <MapEmbed centerAroundCurrentLocation={true} placeIds={places}
-                    zoom={4} />;
-            submissionTable = <Table headers={headers} data={submissions} />;
-        }
 
-        var charts = [];
-        for (var key in this.state.elements){
-            var element = this.state.elements[key];
-            var answers = [];
-            var answerCount = [];
+        if (this.state.form){
+            var title = this.state.form.title;
+            var elements = this.state.form['elements'];
 
             for (var i in this.state.form['submissions']){
                 var submission = this.state.form['submissions'][i]['formsubmission'];
-                if (key in submission['data']){
-                    var answer = submission['data'][key];
-
-                    var index = answers.indexOf(answer);
-                    if (index > -1){
-                        answerCount[index] += 1;
-                    } else {
-                        answers.push(answer);
-                        answerCount.push(1);
-                    }
+                var title = submission['searchTerm'];
+                if ('name' in submission['market']){
+                    title += " : " + submission['market']['name'];
                 }
 
+                var viewButton = <Button css={{marginLeft: "10px", 'display': 'inline-block'}} inline={true} type={'info'}
+                        text={'View'} href={'/project/' + this.props.project + '/results/' + this.props.id + '/submission/' + submission.id + '/'} />;
+                var viewAddress = <div>
+                    {submission['data']['address'] + ' '}
+                    {viewButton}
+                </div>
+
+                submissions.push([
+                    i,
+                    submission['market']['name'],
+                    viewAddress,
+                    submission['data'][elements[11]['formelement']['id']],
+                    submission['data'][elements[7]['formelement']['id']],
+                    '$' + submission['data'][elements[18]['formelement']['id']] + ' - $'  + submission['data'][elements[19]['formelement']['id']],
+                    submission['data'][elements[17]['formelement']['id']],
+                    '$' + submission['data'][elements[20]['formelement']['id']] + ' - $'  + submission['data'][elements[21]['formelement']['id']],
+                ]);
+
+                places.push(submission['data']['placeId']);
             }
 
-            if (answers.length){
-                var chartData = [];
-                for (var i in answers){
-                    chartData.push({
-                        label: String(answers[i]),
-                        value: answerCount[i],
-                        color: CHARTCOLORS[(i%CHARTCOLORS.length)]['color'],
-                        highlight: CHARTCOLORS[(i%CHARTCOLORS.length)]['highlight']
-                    });
+            var headers = [
+                '#',
+                'Buisiness Unit',
+                'Property Address',
+                'Size',
+                'Owned vs. Leased',
+                'NNN Market Rent/SF/YR',
+                'Sale Value - Vacant',
+                'Sale Value - Leaseback'
+            ];
+
+            if (this.state.loaded){
+                map = <MapEmbed centerAroundCurrentLocation={true} placeIds={places}
+                        zoom={4} />;
+                submissionTable = <Table headers={headers} data={submissions} />;
+            }
+
+
+            for (var i in this.state.form['submissions']){
+                var submission = this.state.form['submissions'][i]['formsubmission'];
+
+                data[2] += parseFloat(submission['data'][elements[11]['formelement']['id']]);
+                data[3] += parseFloat(submission['data'][elements[11]['formelement']['id']]);
+                data[4] += parseFloat(submission['data'][elements[27]['formelement']['id']]);
+                data[5] += parseFloat(submission['data'][elements[17]['formelement']['id']]);
+                data[6] += parseFloat(submission['data'][elements[17]['formelement']['id']]);
+            }
+
+            data[3] = data[3] / this.state.form['submissions'].length;
+            data[4] = data[4] / this.state.form['submissions'].length;
+            data[6] = data[6] / this.state.form['submissions'].length;
+            for (var i in data){
+                data[i] = String(data[i]);
+            }
+
+            for (var key in this.state.elements){
+                var element = this.state.elements[key];
+                var answers = [];
+                var answerCount = [];
+
+                for (var i in this.state.form['submissions']){
+                    var submission = this.state.form['submissions'][i]['formsubmission'];
+
+                    if (key in submission['data']){
+                        var answer = submission['data'][key];
+
+                        var index = answers.indexOf(answer);
+                        if (index > -1){
+                            answerCount[index] += 1;
+                        } else {
+                            answers.push(answer);
+                            answerCount.push(1);
+                        }
+                    }
+
                 }
 
-                charts.push(
-                    <div className='col-3'>
-                        <div style={{textAlign: 'left'}}>
-                            {element['pretext']}
+                if (answers.length){
+                    var chartData = [];
+                    for (var i in answers){
+                        chartData.push({
+                            label: String(answers[i]),
+                            value: answerCount[i],
+                            color: CHARTCOLORS[(i%CHARTCOLORS.length)]['color'],
+                            highlight: CHARTCOLORS[(i%CHARTCOLORS.length)]['highlight']
+                        });
+                    }
+
+                    charts.push(
+                        <div className='col-3'>
+                            <div style={{textAlign: 'left'}}>
+                                {element['pretext']}
+                            </div>
+                            <PieChart data={chartData} options={{}}/>
+                            <br/><br/>
                         </div>
-                        <PieChart data={chartData} options={{}}/>
+                    );
+                }
+            }
+
+            content =
+            <div className='container resultPage'>
+                <br/><br/>
+                <div style={{textAlign:"center"}} className='row'>
+                    <div className='col-12' style={{textAlign:'left'}}>
+                        <h1>Results : {title}</h1>
+                        <br/>
+                    </div>
+
+                    <div className='col-12 row'>
+                        <div className='col-6'>
+                            <div className='rollupCard light tall'>
+                                <h1>{data[0]}%</h1>
+                                <div>YOY Vacant Sale Portfolio Value<br/>Increase Since 2016</div>
+                            </div>
+                        </div>
+                        <div className='col-6'>
+                            <div className='rollupCard tall'>
+                                <h1>{data[1]}%</h1>
+                                <div>YOY 10-Year Leaseback Portfolio Sale Value<br/>Increase Since 2016</div>
+                            </div>
+                        </div>
+
+                        <div className='col-12'><br/></div>
+                    </div>
+
+                    <div className='col-12 row'>
+                        <div className='col-4'>
+                            <div className='rollupCard light'>
+                                <h2>{data[2]}</h2>
+                                <div>Total Portfolio SF</div>
+                            </div>
+                        </div>
+                        <div className='col-4'>
+                            <div className='rollupCard'>
+                                <h2>{data[3]}</h2>
+                                <div>Avg. Building SF</div>
+                            </div>
+                        </div>
+                        <div className='col-4'>
+                            <div className='rollupCard dark'>
+                                <h2>{data[4]}%</h2>
+                                <div>Avg. Portfolio Market Vacancy Rate</div>
+                            </div>
+                        </div>
+
+                        <div className='col-4'>
+                            <div className='rollupCard light'>
+                                <h2>${data[5]}</h2>
+                                <div>Portfolio Value Estimate</div>
+                            </div>
+                        </div>
+                        <div className='col-4'>
+                            <div className='rollupCard'>
+                                <h2>${data[6]}</h2>
+                                <div>Avg. Building Value</div>
+                            </div>
+                        </div>
+                        <div className='col-4'>
+                            <div className='rollupCard dark'>
+                                <h2>${data[7]}</h2>
+                                <div>Avg. Portfolio Market Asking<br/> Rate / Year (NNN)</div>
+                            </div>
+                        </div>
+
+                        <div className='col-12'><br/></div>
+                    </div>
+
+                    <div className='col-12 row'>
                         <br/><br/>
                     </div>
-                );
-            }
+
+                    <div className='col-12'>
+                        {submissionTable}
+                    </div>
+
+                    <div className='col-12 row'>
+                        <br/><br/>
+                    </div>
+
+                    <div className='col-12' style={{minHeight:'300px'}}>
+                        {map}
+                    </div>
+
+                    <br/>
+                </div>
+            </div>;
+
         }
-
-
-
-        var content =
-        <div className='container'>
-            <br/><br/>
-            <div style={{textAlign:"center"}} className='row'>
-                <div className='col-12' style={{textAlign:'left'}}>
-                    <h1>Results : {this.state.form.title}</h1>
-                </div>
-
-                <div className='col-12 row'>
-                </div>
-
-                <div className='col-12 row'>
-                    <br/><br/><br/>
-                </div>
-
-                <div className='col-12'>
-                    {submissionCards}
-                </div>
-                <div className='col-12'>
-                    {submissionTable}
-                </div>
-                <div className='col-12' style={{minHeight:'300px'}}>
-                    {map}
-                </div>
-
-                <br/>
-            </div>
-        </div>;
 
         return (
             <Wrapper token={this.props.user_id} loaded={this.state.loaded}  content={content} />
