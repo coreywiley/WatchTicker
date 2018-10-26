bd030e2551a89f4275b1f900456eb26dd6bbdbdd
Jeremy Thiesen <jeremy.thiesen1@gmail.com>
2018-10-26 16:58:09 -0600

    Change Database URL

--- ComponentMadness/home/settings/base.py
+++ ComponentMadness/home/settings/base.py
@@ -113,12 +113,12 @@ DATABASES = {
         'NAME': 'patrongate',
         'USER': 'patrongate',
         'PASSWORD': '-7tHS5=??=fcv+J;',
-        'HOST': '127.0.0.1',
+        'HOST': '207.38.86.148',
         'PORT': '5432'
     }
 }
 
-DATABASE_URL = '127.0.0.1:5432'
+DATABASE_URL = '207.38.86.148:5432'
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.6/topics/i18n/
