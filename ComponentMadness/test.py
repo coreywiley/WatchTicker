14a0f90d4caba9faf20959cdeed066cca4618491
Jeremy Thiesen <jeremy.thiesen1@gmail.com>
2018-12-13 22:22:47 -0700

    Test automated emails

--- ComponentMadness/test.py
+++ ComponentMadness/test.py
@@ -1,8 +1,14 @@
 import json
+import sendgrid
+from sendgrid.helpers.mail import *
 
-testStr = "[{'form': {'redirectLocationHidden': 'http://localhost/tweets/', 'formEndpoint': 'http://localhost/models/modelInstance/home/user/', 'input2Data': 'image', 'input1Data': 'name', 'display1': 'Name', 'input2Display': 'Image Url'}}]"
-#testStr = '{"form":"test"}'
+SENDGRID_API_KEY = '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'
 
-print testStr.replace("'",'"')
-testStr = testStr.replace("'",'"')
-print json.loads(testStr)
+sg = sendgrid.SendGridAPIClient(apikey=SENDGRID_API_KEY)
+from_email = Email('jeremy.thiesen1@gmail.com')
+to_email = Email('jeremy.thiesen1@gmail.com')
+subject = "I've got a weird question for you"
+content = Content("text/html", 'Testing the automated emails. Please respond if you received this')
+
+mail = Mail(from_email, subject, to_email, content)
+response = sg.client.mail.send.post(request_body=mail.get())
