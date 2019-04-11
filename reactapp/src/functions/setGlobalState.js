52097fa9f3b2529bc3e2b5dc420ab8bf220807cb
Jeremy Thiesen <jeremy.thiesen1@gmail.com>
2019-04-10 22:05:32 -0600

    Added the ability to subscribe and get updates just like redux

--- reactapp/src/functions/setGlobalState.js
+++ reactapp/src/functions/setGlobalState.js
@@ -1,7 +1,15 @@
-window.cmState = {}
+window.cmState = {'test':'test'}
 
 function setGlobalState(name, state) {
   window.cmState[name] = state;
+  if (window.cmState.subscribers) {
+    for (var index in window.cmState.subscribers) {
+      var component = window.cmState.subscribers[index];
+      console.log(component.constructor.name)
+      component.forceUpdate();
+    }
+  }
+  
   return true
 }
 
@@ -13,6 +21,20 @@ function getGlobalState(name) {
     return window.cmState
   }
 }
+
+function subscribe(component) {
+  if (!window.cmState['subscribers']) {
+    window.cmState['subscribers'] = [];
+  }
+  window.cmState['subscribers'].push(component)
+}
+
+/*
+Put the following in your react component constructor or really anywhere that runs
+window.cmState.subscribe(this);
+*/
+
+window.cmState['subscribe'] = subscribe;
 
 window.cmState['setGlobalState'] = setGlobalState;
 window.cmState['getGlobalState'] = getGlobalState;
