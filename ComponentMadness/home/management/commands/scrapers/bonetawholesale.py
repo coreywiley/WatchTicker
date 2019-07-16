13a984c343e9afb6c2e505dae87a9795223961c0
Irakli <igugu13@freeuni.edu.ge>
2019-07-16 15:26:16 -0700

    update bonetawholesale

--- ComponentMadness/home/management/commands/scrapers/bonetawholesale.py
+++ ComponentMadness/home/management/commands/scrapers/bonetawholesale.py
@@ -2,37 +2,43 @@ import requests
 from bs4 import BeautifulSoup
 from home.management.commands.scrapers.basicSpider import BasicSpider
 
-not_needed_brand_urls = ['https://bonetawholesale.com/SubCategory/ROLEX/2912',
-                         'https://bonetawholesale.com/SubCategory/OYSTER%20PERPETUAL%2026MM/3284',
-                         'https://bonetawholesale.com/SubCategory/SEA-DWELLER%20DEEP%20SEA/2999',
-                         'https://bonetawholesale.com/SubCategory/BREGUET/2919',
-                         'https://bonetawholesale.com/SubCategory/BLANCPAIN/3056',
-                         'https://bonetawholesale.com/SubCategory/CARL%20F.%20BUCHERER/3724',
-                         'https://bonetawholesale.com/SubCategory/FREDERIQUE%20CONSTANT/3421',
-                         'https://bonetawholesale.com/SubCategory/LECOULTRE/2951',
-                         'https://bonetawholesale.com/SubCategory/MAITRE%20DU%20TEMPS/4672',
-                         'https://bonetawholesale.com/SubCategory/CHOPARD/3340',
-                         'https://bonetawholesale.com/SubCategory/GUCCI/3347',
-                         'https://bonetawholesale.com/SubCategory/PIAGET/3177',
-                         'https://bonetawholesale.com/SubCategory/BULGARI/3178',
-                         'https://bonetawholesale.com/SubCategory/POMELLATO/3179',
-                         'https://bonetawholesale.com/SubCategory/MISC/3181',
-                         'https://bonetawholesale.com/SubCategory/ROBERTO%20COIN/3243',
-                         'https://bonetawholesale.com/SubCategory/CARTIER/3241',
-                         'https://bonetawholesale.com/SubCategory/DAMIANI/3189']
-
 
 class BonetaWholesale(BasicSpider):
+
     @staticmethod
-    def get_needed_brand_urls(max_needed_urls):
+    def get_needed_brand_urls():
         response = requests.get("https://bonetawholesale.com/")
         soup = BeautifulSoup(response.text, "html.parser")
-        my_brand_urls = ["https://bonetawholesale.com" + subcategory["href"].replace(" ", "%20")
-                         for subcategory in soup.select("ul.TreeNodeChild li a")]
-        needed_brand_urls = [brand_url for brand_url in my_brand_urls if brand_url not in not_needed_brand_urls]
-        if max_needed_urls:
-            needed_brand_urls = needed_brand_urls[:max_needed_urls]
+        needed_brand_urls = ["https://bonetawholesale.com" + subcategory["href"].replace(" ", "%20")
+                             for subcategory in soup.select("ul.TreeNodeChild li a")]
         return needed_brand_urls
+
+    @staticmethod
+    def send_pagination_request(url, page, viewstate):
+        headers = {
+            "Connection": "keep-alive",
+            "Cache-Control": "max-age=0",
+            "Origin": "https://bonetawholesale.com",
+            "Upgrade-Insecure-Requests": "1",
+            "DNT": "1",
+            "Content-Type": "application/x-www-form-urlencoded",
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) "
+                          "Chrome/75.0.3770.100 Safari/537.36",
+            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;"
+                      "q=0.8,application/signed-exchange;v=b3",
+            "Accept-Encoding": "gzip, deflate, br",
+            "Accept-Language": "en-US,en;q=0.9",
+        }
+
+        data = [
+            ("__EVENTTARGET", "ItemList4$AspNetPager1"),
+            ("__EVENTARGUMENT", str(page)),
+            ("__VIEWSTATE", viewstate),
+            ("__VIEWSTATEENCRYPTED", "")
+        ]
+
+        response = requests.post(url, headers=headers, data=data)
+        return response
 
     @staticmethod
     def get_watch_info(url, watch):
@@ -55,35 +61,65 @@ class BonetaWholesale(BasicSpider):
                 "wholesale": True}
         return info
 
-    def getWatches(self, max_needed_urls=None):
-        brand_urls = self.get_needed_brand_urls(max_needed_urls)
+    @staticmethod
+    def get_next_page_viewstate(soup):
+        watch_count_text = soup.select("#lblItemCount")[0].text.strip()
+        cur_watch_count = int(watch_count_text.split("-")[1].split(" ")[0].strip())
+        max_watch_count = int(watch_count_text.split(" ")[-1].strip())
+        if cur_watch_count == max_watch_count:
+            return None
+        viewstate = soup.select("#__VIEWSTATE")[0]["value"]
+        return viewstate
+
+    def getWatches(self):
+        brand_urls = self.get_needed_brand_urls()
         for url in brand_urls:
             print(url)
             response = requests.get(url)
-            soup = BeautifulSoup(response.text, features="html.parser")
-            try:
-                watches = soup.select("div.prod-desc")
-                for watch in watches:
-                    info = self.get_watch_info(url, watch)
-                    info.pop("sold")
-                    info.pop("wholesale")
-                    yield info
+            page = 2
+            while 1:
+                try:
+                    soup = BeautifulSoup(response.text, features="html.parser")
+                    watches = soup.select("div.prod-desc")
+                    for watch in watches:
+                        info = self.get_watch_info(url, watch)
+                        info.pop("sold")
+                        info.pop("wholesale")
+                        yield info
 
-            except Exception as e:
-                print(str(e))
-                self.sendErrorEmail("Boneta Wholesale", "getWatches: " + url, str(e))
-                yield {"error": True, "error_detail": str(e)}
+                    viewstate = self.get_next_page_viewstate(soup)
+                    if not viewstate:
+                        break
+                    response = self.send_pagination_request(url, page, viewstate)
+                    page += 1
+
+                except Exception as e:
+                    print(str(e))
+                    self.sendErrorEmail("Boneta Wholesale", "getWatches: " + url, str(e))
+                    yield {"error": True, "error_detail": str(e)}
+                    break
 
     def getWatchDetails(self, url):
         response = requests.get(url)
-        soup = BeautifulSoup(response.text, features="html.parser")
 
-        watches = soup.select("div.prod-desc")
-        for watch in watches:
-            info = self.get_watch_info(url, watch)
-            if info["url"] == url:
-                info.pop("url")
-                return {"sold": True} if info["sold"] else info
+        if response.status_code == 404:
+            return {"sold": True}
+
+        page = 2
+        while 1:
+            soup = BeautifulSoup(response.text, features="html.parser")
+            watches = soup.select("div.prod-desc")
+            for watch in watches:
+                info = self.get_watch_info(url, watch)
+                if info["url"] == url:
+                    info.pop("url")
+                    return {"sold": True} if info["sold"] else info
+
+            viewstate = self.get_next_page_viewstate(soup)
+            if not viewstate:
+                break
+            response = self.send_pagination_request(url, page, viewstate)
+            page += 1
 
 
 # BonetaWholesale().do_testing()
